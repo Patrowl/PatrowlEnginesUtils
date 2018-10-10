5ea1d4e8f2a52b9b0c2586b8676292fe53bac158
Nicolas Mattiocco <nicolas@greenlock.fr>
2018-10-10 16:56:50 +0200

    PyPi packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PatrowlEnginesUtils',
+    version='0.0.1',
+    description='Common classes for PatrowlEngines',
+    url='https://github.com/Patrowl/PatrowlEnginesUtils',
+    author='Nicolas Mattiocco',
+    author_email='nicolas@patrowl.io',
+    license='AGPLv3',
+    packages = find_packages(),
+)
