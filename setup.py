3dc310eda3e87a9fde37dcfdf1a7d57690541231
Nicolas Mattiocco <nicolas@greenlock.fr>
2020-06-01 09:33:14 +0200

    Update version to 0.1.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PatrowlEnginesUtils',
-    version='0.1.1',
+    version='0.1.2',
     description='Common classes for PatrowlEngines',
     url='https://github.com/Patrowl/PatrowlEnginesUtils',
     author='Nicolas Mattiocco',
