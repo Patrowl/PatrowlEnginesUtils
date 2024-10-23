e48e51ccf21ec279e9b1accf07899551fcdda793
sebastien <sebastien.coll+integration@patrowl.io>
2024-10-23 15:32:44 +0200

    Updated VERSION (1.2.1rc1)

--- PatrowlEnginesUtils/__init__.py
+++ PatrowlEnginesUtils/__init__.py
@@ -3,7 +3,7 @@
 
 
 __title__ = 'patrowl_engine_utils'
-__version__ = '1.2.0'
+__version__ = '1.2.1rc1'
 __author__ = 'Nicolas MATTIOCCO'
 __license__ = 'AGPLv3'
 __copyright__ = 'Copyright (C) 2018-2021 Nicolas Mattiocco - @MaKyOtOx'
